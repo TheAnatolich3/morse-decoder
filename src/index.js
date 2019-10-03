aa642f9e8daa5b499870051823a09ba750b424fb
Evgeniy Zakrevskiy <zakrevskii3112@gmail.com>
2019-10-03 23:30:41 +0300

    feat: add morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let final_result = '';
+    for(let cur = 10, prev = 0; prev < expr.length; prev+=10, cur+=10){
+        let s = expr.substring(prev, cur);
+        let res = '';
+        for(i = 0; i < 5; i++){
+            let symbol = s.substring(i*2, (i+1)*2);
+            if (symbol == '10'){
+                res += '.';
+            }
+            else if(symbol == '11'){
+                res += '-';
+            }
+            else{
+                continue;
+            }
+        }
+        if(res == ''){
+            final_result += ' ';
+        }
+        else{
+            final_result += MORSE_TABLE[res];
+        }
+    }
+    return final_result;
 }
 
 module.exports = {
